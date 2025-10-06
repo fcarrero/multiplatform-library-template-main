1ce6996c51c56897f0c87b80028c5dafce96c91c
Franklin <franklin.carrero@conekta.com>
2025-10-06 16:12:43 -0500

    f

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,5 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
