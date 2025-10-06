7b2f61a3de82dcfa6cf1cf0b78c740874b2461c0
Franklin <franklin.carrero@conekta.com>
2025-10-06 15:48:18 -0500

    a

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,10 +7,9 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven { url = uri("https://jitpack.io") }
     }
 }
